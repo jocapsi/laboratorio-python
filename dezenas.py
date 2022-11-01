fe353eef3431fba98fafb02e28c29f93c8e661dc
Jonathan Tavares <108154259+jonathan-tavares@users.noreply.github.com>
2022-11-01 13:33:21 -0300

    conversor dezenas
    conversor de inteiro para dezenas

---
+++ dezenas.py
@@ -0,0 +1,11 @@
+ninteiro=int(input("Digite um número inteiro:"))
+
+calc0=int(ninteiro%10)
+
+calc1=int(ninteiro-calc0)
+
+calc2=int(calc1/10)
+
+dezena=int(calc2%10)
+
+print("O dígito das dezenas é", dezena)
