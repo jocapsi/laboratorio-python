1865bc23f3c656134333dc1884cadbd0467e8bc0
Jonathan Tavares <108154259+jonathan-tavares@users.noreply.github.com>
2022-11-12 14:48:08 -0300

    ordenando

---
+++ ordernando.py
@@ -0,0 +1,7 @@
+receba = int(input("Digite o primeiro número inteiro"))
+receba2 = int(input("Digite o segundo número inteiro"))
+receba3 = int(input("Digite o terceiro número inteiro"))
+if receba <= receba2 and receba2 <= receba3:
+    print("crescente")
+else:
+    print("não está em ordem crescente")
