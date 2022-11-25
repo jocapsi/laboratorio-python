a1f0c23bbc69b89cbc49d61549896eedd468d8b4
Jonathan Tavares <108154259+jonathan-tavares@users.noreply.github.com>
2022-11-25 09:41:29 -0300

    Números ímpares

---
+++ n_impares.py
@@ -0,0 +1,11 @@
+# Número inteiro positivo
+
+receba = int(input("Digite o valor de n: "))
+
+zero = 0
+impar = 1
+
+while zero < receba:
+    print(impar)
+    zero = zero + 1
+    impar = impar + 2
