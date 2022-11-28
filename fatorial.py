d2af6a376672514f6d0c26b49d71e41156129869
Jonathan Tavares <108154259+jonathan-tavares@users.noreply.github.com>
2022-11-28 09:33:05 -0300

    DEF Função Fatorial
    Exemplo de DEF (função) para implementação de fatorial e distribuições binomiais.

--- fatorial.py
+++ fatorial.py
@@ -1,11 +1,25 @@
-#Printing fatorial numbers
+#Fatoriais e binomiais
 
-entrada = int(input("Digite o valor de n: "))
+def fatorial(n):
+    fat = 1
+    while (n > 1):
+        fat = fat * n
+        n = n - 1
+    return fat
 
-resultado = 1
-count = 1
+def numero_binomial(n, k):
+    return fatorial(n) / (fatorial(k) * fatorial(n-k))
 
-while count <= entrada:
-    resultado *= count
-    count += 1
-print(resultado)
+def testa_fatorial():
+    if fatorial(1) == 1:
+        print("Funciona para 1")
+    else:
+        print("Não funciona para 1")
+    if fatorial(0) == 1:
+        print("Funciona para 0")
+    else:
+        print("Não funciona para 0")
+    if fatorial(5) == 120:
+        print("Funciona para 5")
+    else:
+        print("Não funciona para 5")
