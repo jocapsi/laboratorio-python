3d4ccfbe5440bd3376c7e63b410f2649ed136d49
Jonathan Tavares <108154259+jonathan-tavares@users.noreply.github.com>
2023-01-29 16:31:11 -0300

    Imprime_lista_chamada
    Imprime uma lista escolhida

---
+++ imprime_lista_chamada.py
@@ -0,0 +1,22 @@
+#content
+
+var1 = ("ffdfddf", "fgffg", "gfgf", "fg", "fg", "fg", "fg", "fd", "ggj", "dfff", "g", "hhjk")
+
+var2 = ("htjhjk", "hghgk", "tragic", "ghklfgj", "mosquito", "ghjlkk", "hhkfhkg", "ffjkjrtytuy", "jykulk", "tjykuli", "rutykuli", "rutyu", "rtyu", "tyukl","rtyukl")
+
+var3 = ["rtyujk", "rty", "rtyukl", "rtyukl", "ertyujk", "dftyjkl", "rtyukl;", "sdfghj", "dfggggh", "dfgh", "dfghjk", "dfghjk"]
+
+passely = ("fdfddfdfd")
+passjonathan = ("dggddg-br")
+
+#input
+
+seed = str(input("Escolha qual seed gostaria de exibir: eth, btc, visualnew, block4u, btcely, sol, btcvisualnew, btc, btcpublic, pietrok ou fatcat"))
+if seed == "var1":
+    print (var1)
+if seed == "var2":
+    print (var2)
+if seed == "var3":
+    print (var3)
+else:
+    print("pass 1:", passely, "pass Jonathan:", passjonathan)
